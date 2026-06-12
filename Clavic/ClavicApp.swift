ff8744fcb09f0062b29f49b222a4152d1c6f4c20
NJscalp <normannjungbauer3@gmail.com>
2026-06-13 01:40:09 +0200

    Initial Commit

---
+++ Clavic/ClavicApp.swift
@@ -0,0 +1,32 @@
+//
+//  ClavicApp.swift
+//  Clavic
+//
+//  Created by Normann Jungbauer on 13.06.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ClavicApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
